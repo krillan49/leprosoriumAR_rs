59d623043347d85a928d4c3b628faf489468f997
krillan <pushkinidze49@gmail.com>
2023-06-03 10:27:59 +0300

    созданы модели миграции и таблицы posts and comments

--- app.rb
+++ app.rb
@@ -3,3 +3,9 @@ require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
 set :database, { adapter: 'sqlite3', database: 'leprosorium.db' }
+
+class Post < ActiveRecord::Base
+end
+
+class Comment < ActiveRecord::Base
+end
