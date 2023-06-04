16f1d14a5ce492ca0c2d2779e3c7c9a52ee8b3eb
krillan <pushkinidze49@gmail.com>
2023-06-04 06:56:37 +0300

    добавлены универсальные гет и пост обработчики для /details/:post_id и базовый код и валидация формы в details.erb

--- app.rb
+++ app.rb
@@ -10,6 +10,7 @@ class Post < ActiveRecord::Base
 end
 
 class Comment < ActiveRecord::Base
+  validates :content, presence: true
 end
 
 get '/' do
@@ -23,11 +24,28 @@ get '/new' do
 end
 
 post '/new' do
-  @post = Post.new(params[:post])
+  @post = Post.new(params[:add_post])
   if @post.save
     redirect to '/'
   else
     @error = @post.errors.full_messages
     erb :new
   end
+end
+
+get '/details/:post_id' do
+  @post = Post.find(params[:post_id])
+  @comments = Comment.where("post_id = ?", params[:post_id])
+  erb :details
+end
+
+post '/details/:post_id' do
+  comment = Comment.new
+  comment.content = params[:content]
+  comment.post_id = params[:post_id]
+  comment.save
+
+  @post = Post.find(params[:post_id])
+  @comments = Comment.where("post_id = ?", params[:post_id])
+  erb :details
 end
