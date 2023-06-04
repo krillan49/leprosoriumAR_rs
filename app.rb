0651a206f8f79f2b250ba8cc702df01abe332bda
krillan <pushkinidze49@gmail.com>
2023-06-04 05:36:28 +0300

    добавлена базовая валидация для формы из new.erb

--- app.rb
+++ app.rb
@@ -5,6 +5,8 @@ require 'sinatra/activerecord'
 set :database, { adapter: 'sqlite3', database: 'leprosorium.db' }
 
 class Post < ActiveRecord::Base
+  validates :content, length: { minimum: 100 }
+  validates :author, presence: true
 end
 
 class Comment < ActiveRecord::Base
@@ -16,11 +18,16 @@ get '/' do
 end
 
 get '/new' do
+  @post = Post.new
   erb :new
 end
 
 post '/new' do
-  post = Post.new(params[:post])
-  post.save
-  redirect to '/'
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect to '/'
+  else
+    @error = @post.errors.full_messages
+    erb :new
+  end
 end
